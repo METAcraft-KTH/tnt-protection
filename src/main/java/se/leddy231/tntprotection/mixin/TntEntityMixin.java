be1bc679e061d2394543783cfcd53a2b495d8e4b
Martin Larsson <martin.99.larsson@telia.com>
2022-02-06 22:44:28 +0100

    Allow breaking blocks on nether roof

--- src/main/java/se/leddy231/tntprotection/mixin/TntEntityMixin.java
+++ src/main/java/se/leddy231/tntprotection/mixin/TntEntityMixin.java
@@ -2,6 +2,7 @@ package se.leddy231.tntprotection.mixin;
 
 import net.minecraft.entity.TntEntity;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraft.world.explosion.Explosion;
 import se.leddy231.tntprotection.ProtectionStateManager;
 
@@ -12,14 +13,28 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TntEntity.class)
 public class TntEntityMixin {
+
+	boolean isWilderness(BlockPos pos, World dimension) {
+		return pos.getZ() <= -5;
+	}
+
+	boolean isNetherRoof(BlockPos pos, World dimension) {
+		return dimension.getRegistryKey().equals(World.NETHER) && pos.getY() >= 120;
+	}
+
+	boolean isWhitelistedArea(BlockPos pos, World dimension) {
+		return ProtectionStateManager.instance.isWhitelisted(pos, dimension);
+	}
+
 	@Inject(method = "explode()V", at = @At("HEAD"), cancellable = true)
     private void explode(CallbackInfo ci) {
 		TntEntity e = (TntEntity) (Object) this;
 		BlockPos pos = e.getBlockPos();
-		if (pos.getZ() > -5 && !ProtectionStateManager.instance.isWhitelisted(pos, e.getEntityWorld())) {
-			ci.cancel();
-        	e.world.createExplosion(e, e.getX(), e.getBodyY(0.0625), e.getZ(), 4.0f, Explosion.DestructionType.NONE);
+		World dimension = e.getEntityWorld();
+		if (isWilderness(pos, dimension) || isNetherRoof(pos, dimension) || isWhitelistedArea(pos, dimension)) {
+			return;
 		}
-        
+		ci.cancel();
+		e.world.createExplosion(e, e.getX(), e.getBodyY(0.0625), e.getZ(), 4.0f, Explosion.DestructionType.NONE);
     }
 }
